743ed999bf8c59b56407371ca196092e90afabc8
Harry <hcnode@gmail.com>
2018-01-17 15:47:18 +0800

    update

--- channels/xigua.js
+++ channels/xigua.js
@@ -3,15 +3,19 @@ module.exports = {
     if (item.color == "white") {
       pct = Math.round(item.count / total * 100);
       console.log(`total:${total} - ${pct}`);
-      if (pct > 40) {
+      if (pct > 80) {
         return true;
       }
     }
   },
   crop: {
+    // 截图开始的点的x坐标
     x: 30,
+    // 截图开始的点的y坐标
     y: 117,
+    // 截图的宽
     cropwidth: 439,
+    // 截图的高
     cropheight: 440
   }
 };
